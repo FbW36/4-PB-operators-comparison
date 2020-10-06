02816f3e232d9b4e33d4eac2d489c3cfe2428bf9
Zarzarius <zarzarius@gmail.com>
2020-10-07 01:12:04 +0200

    initial commit

---
+++ Azael/index.js
@@ -0,0 +1,50 @@
+// 0. Declare two variables "apples" and "oranges". Assign a value of 20 to apples. Assign a value of 30 to oranges.
+let apples = 20;
+
+let oranges = 30;
+
+// 1. Check whether apples and oranges are equal.
+
+console.log(oranges == apples);
+
+// 2. Check whether apples and oranges are *not* equal.
+
+console.log(oranges != apples);
+
+// 3. Check whether apples is greater than oranges.
+
+console.log(apples > oranges);
+
+// 4. Check whether apples is less than or equal to oranges.
+
+console.log(apples <= oranges);
+
+// 5. Check whether oranges is greater than apples.
+
+console.log(oranges > apples);
+
+// 6. Declare another variable "mangoes" and give it a value of 5. Multiply mangoes and apples, and check whether this result is greater than mangoes added to oranges.
+
+let mangoes = 5;
+
+let mal = mangoes * apples;
+
+console.log(mal);
+
+console.log(mal > mangoes + apples);
+
+// 7. Subtract mangoes from apples and check whether this result is less than oranges divided by mangoes.
+
+let rest = apples - oranges;
+
+console.log(rest < oranges / mangoes);
+
+// 8. Check whether mangoes, apples and oranges are equal.
+
+let;
+
+// 9. Check whether the remainder of apples divided by mangoes and the remainder of oranges divided by mangoes are equal.
+
+// 10. Check whether mangoes added to apples is greater than oranges minus mangoes. If it is not, find an operator which will give a result of true.
+
+// 11. BONUS CHALLENGE: Try increasing script readability by adding question headings to your output before each result.
