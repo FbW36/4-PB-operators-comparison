efb76f445bb1d221b7bee8a17c57b46b6b6006a3
Sara <you@example.com>
2020-10-06 15:59:42 +0200

    exercise1 (1-7 done)

---
+++ Sara/index.js
@@ -0,0 +1,30 @@
+// = to assign ; 
+// == loose equality operator
+// === strict equality operator -java asking that the value is equel on both sides 
+
+//data type coercion 7 and an other data type 7
+
+let apples = 20 ;
+let oranges = 30 ; 
+
+// 1
+console.log(apples === oranges ? 'true' : 'false') ; 
+
+// 2
+console.log(apples !== oranges ? 'true' : 'false') ; 
+
+// 3
+console.log(apples > oranges ? 'true' : 'false') ; 
+
+// 4
+console.log(apples <= oranges ? 'true' : 'false') ; 
+
+// 5
+console.log(apples >! oranges ? 'true' : 'fasle') ; 
+
+// 6
+let mangoes = 5 ; 
+console.log(mangoes * apples > mangoes + oranges ? 'true' : 'false') ;
+
+// 7
+console.log(mangoes - apples < oranges / mangoes ? 'true' : 'false') ; 
