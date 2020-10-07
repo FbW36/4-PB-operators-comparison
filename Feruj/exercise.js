ecd8b43897f2bba5d5afad468c08fa1e60bca10f
Feruj Ahammed <ferujahammed@gmail.com>
2020-10-07 13:18:56 +0200

    secound commit

--- Feruj/exercise.js
+++ Feruj/exercise.js
@@ -2,16 +2,16 @@
 let apples = 20;
 let orange = 30;
 //1, 2
-let checkEqual = apples == orange ? "true" : 'false';
-console.log(checkEqual); //if apples and orange are equal- is true else false. 
+let checkEqual = apples == orange ? "true" : "false";
+console.log(checkEqual); //if apples and orange are equal- is true else false.
 //3
-let checkEqual1 = apples > orange ? "true" : 'false';
+let checkEqual1 = apples > orange ? "true" : "false";
 console.log(checkEqual1); //if apples is greater than orange- is true else false.
 // 4
-let checkEqual2 = apples <= orange ? "true" : 'false';
+let checkEqual2 = apples <= orange ? "true" : "false";
 console.log(checkEqual2); // if apples is smaller or equal than orange- is true else false.
-// 5 
-let checkEqual3 = apples < orange ? "true" : 'false';
+// 5
+let checkEqual3 = apples < orange ? "true" : "false";
 console.log(checkEqual3); // if apples is smaller than orange- is true else false.
 
 // 6
@@ -20,17 +20,17 @@ let mA = mangoes * apples;
 let mO = orange + mangoes;
 console.log(mO > mA); // if mangoes multyply with apples is greater than mangoes and orange- is true else false.
 //7
-let manApp = apples - mangoes;// Subtract
+let manApp = apples - mangoes; // Subtract
 let orngMango = orange / mangoes; // Devided
 console.log(manApp < orngMango); // Smaller than
 //8
-console.log(mangoes == apples == orange); // comparison
+console.log((mangoes == apples) == orange); // comparison
 //9
-let appreminder = apples % 2; //reminder
-let oraReminder = orange % 2;
-console.log((appreminder / mangoes) == (oraReminder / mangoes));// comparison
+let appreminder = apples % mangoes; //reminder
+let oraReminder = orange % mangoes;
+console.log(appreminder == oraReminder); // comparison
 
 //10
 let totalManApp = mangoes + apples;
 let totalOraMang = orange - mangoes;
-console.log(totalManApp > totalOraMang); // greater than
+console.log(totalManApp >= totalOraMang); // greater than
