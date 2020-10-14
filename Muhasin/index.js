1326645838b49add2ae5291ab80d9fce217ff86e
Muhasin <mahmudmuhasin@gmail.com>
2020-10-14 19:52:17 +0200

    rewrite code

--- Muhasin/index.js
+++ Muhasin/index.js
@@ -7,12 +7,12 @@ let mangos = 5;
 console.log(apples == oranges); // == false
 
 // Are apples and oranges unequal ?
-console.log(apples == oranges); // == true
+console.log(apples !== oranges); //
 // are apples greater than oragnes ?
 console.log(apples > oranges); //false
 
 // are apples less than oragnes ?
-console.log(apples < oranges); // true
+console.log(apples <= oranges); // 
 
 // are oranges greater than apples?
 console.log(oranges > apples); // true
@@ -25,11 +25,12 @@ console.log(sumApMa > sumMaOr); // ===>true
 
 console.log(apples - mangos < oranges / mangos); // false
 // are oranges equal to mangos and apples?
-console.log(mangos === apples && mangos === oranges && apples === oranges); // false
+/* console.log(mangos === apples && mangos === oranges && apples === oranges); // false */
+console.log(apples == oranges == mangos);
 
 // is the remainder of apples divided by mangos eual to the remainder of oranges divided by mangos?
 console.log(apples % mangos == oranges % mangos); // == true 
 
 // is mangos added to apples greater than oranges minus mangos?
 console.log(mangos + apples > oranges - mangos); //false 
-console.log(mangos + apples === oranges - mangos); // true 
+console.log((mangos + apples) >= (oranges - mangos)); // ture
